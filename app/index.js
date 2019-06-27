c8a947a42e20c8f3eae396164d369eb8a6750c94
Tomasz Libich <t.libich@gmail.com>
2019-06-27 11:08:32 +0100

    Replace DB variables with connection string

--- app/index.js
+++ app/index.js
@@ -4,17 +4,9 @@ const bodyParser = require("body-parser");
 const routes = require("./routes");
 const app = express();
 const cors = require("cors");
-const {
-  MONGODB_USER,
-  MONGODB_PASSWORD,
-  MONGODB_ADDRESS,
-  MONGODB_NAME
-} = process.env;
+const { MONGODB_CONNECTION_STRING } = process.env;
 
-mongoose.connect(
-  `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_ADDRESS}/${MONGODB_NAME}`,
-  { useNewUrlParser: true }
-);
+mongoose.connect(MONGODB_CONNECTION_STRING, { useNewUrlParser: true });
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB...");
